d9252646561d74c60cf1972ffe6714c2e969e219
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-05-16 11:21:44 -0300

    fix(humor): style

--- projects/humor/js/script.js
+++ projects/humor/js/script.js
@@ -2,53 +2,24 @@ const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 function change(value){
 	if(value == "happy" || value == "angry"){
-		if(vw<vh){
-			document.getElementById("luffy").style.width = "auto"; 
-			document.getElementById("luffy").style.height = "100%"; 
-			document.getElementById("luffy").style.left = "0"; 
-			document.getElementById("name").innerHTML = "Monkey D' Luffy "+value;
-			if(value=="angry"){
-				document.getElementById("name").style.color="red";
-			}
-			else{
-				document.getElementById("name").style.color="lime";
-			}
-		}else{
-			document.getElementById("luffy").style.width = "100%"; 
-			document.getElementById("luffy").style.height = "auto"; 
-			document.getElementById("luffy").style.left = "0"; 
-			document.getElementById("name").innerHTML = "Monkey D' Luffy "+value;
-			if(value=="angry"){
-				document.getElementById("name").style.color="red";
-			}
-			else{
-				document.getElementById("name").style.color="lime";
-			}
+	
+		document.getElementById("name").innerHTML = "Monkey D' Luffy "+value;
+		if(value=="angry"){
+			document.getElementById("name").style.color="red";
 		}
+		else{
+			document.getElementById("name").style.color="lime";
+		}
+
 	}else{
-		if(vw<vh){
-			document.getElementById("luffy").style.width = "auto"; 
-			document.getElementById("luffy").style.height = "50%"; 
-			document.getElementById("luffy").style.left = "10%"; 
-			document.getElementById("name").innerHTML = "Monkey D' Luffy "+value;
-			if(value=="idiot"){
-				document.getElementById("name").style.color="blue";
-			}
-			else{
-				document.getElementById("name").style.color="yellow";
-			}
-		}else{
-			document.getElementById("luffy").style.width = "50%"; 
-			document.getElementById("luffy").style.height = "auto"; 
-			document.getElementById("luffy").style.left = "10%"; 
-			document.getElementById("name").innerHTML = "Monkey D' Luffy "+value;
-			if(value=="idiot"){
-				document.getElementById("name").style.color="blue";
-			}
-			else{
-				document.getElementById("name").style.color="yellow";
-			}
+		document.getElementById("name").innerHTML = "Monkey D' Luffy "+value;
+		if(value=="idiot"){
+			document.getElementById("name").style.color="blue";
 		}
+		else{
+			document.getElementById("name").style.color="yellow";
+		}
+		
 	}
 	document.getElementById("luffy").src="imgs/"+value+".png";
 	console.log("img/"+value+".png");
